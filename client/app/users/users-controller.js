877104b7b91cc243f5bf2429ad084469dea39f16
Angelina <angelina.may@hotmail.com>
2016-09-07 15:54:35 -0700

    [feature]signup form validates password match and empty fields

--- client/app/users/users-controller.js
+++ client/app/users/users-controller.js
@@ -6,16 +6,29 @@ angular.module('app.users', [])
       status:true,
       issues:[]
     }
-    if(pw !== pw2) {
-      validated.status = false
+    var args = ['firstname', 'lastname', 'email', 'username', 'password', 'confirm password']
+    for(var i = 0; i < arguments.length; i++){
+      if(!arguments[i]){
+        $scope.validated.status = false;
+        $scope.validated.issues.push("missing " + args[i])
+      }
     }
+    if(pw !== pw2) {
+      $scope.validated.status = false
+      $scope.validated.issues.push("passwords do not match")
+    }
+
     if($scope.validated.status){
       $state.go('login')
     }
-  }
-
-  $scope.login = function(){
-    $state.go('login')
+    else{
+      var issues = "";
+      $scope.validated.issues.forEach(function(x){
+        issues += x + "\n"
+      })
+      console.log($scope.validated.issues, "***", issues)
+      alert('please correct the below issues' + "\n" + issues)
+    }
   }
 
 }])
