ffef76e4439ddbadca50d41e3e05b4ea7c1e3d07
Angelina <angelina.may@hotmail.com>
2016-09-07 15:40:01 -0700

    [feature]signup redirects to login

--- client/app/users/users-controller.js
+++ client/app/users/users-controller.js
@@ -1,4 +1,21 @@
 angular.module('app.users', [])
 .controller('Users', ['$scope', '$state', 'Users', function($scope, $state, Users) {
 
+  $scope.signupUser = function (first, last, email, user, pw, pw2) {
+    $scope.validated = {
+      status:true,
+      issues:[]
+    }
+    if(pw !== pw2) {
+      validated.status = false
+    }
+    if($scope.validated.status){
+      $state.go('login')
+    }
+  }
+
+  $scope.login = function(){
+    $state.go('login')
+  }
+
 }])
